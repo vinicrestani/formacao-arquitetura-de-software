53931c1bb8116ca8f9f44c5da19e133b087150f7
Vinicius Crestani <viniciuscrestani.compasso@natura.net>
2026-08-28 11:20:17 -0300

    Projeto - Automacao Testes

--- 01-automacao-de-testes/src/validateCpf.ts
+++ 01-automacao-de-testes/src/validateCpf.ts
@@ -1,60 +1,19 @@
-export function validateCpf (str: any) {
-	if (str !== null) {
-		if (str !== undefined) {
-			if (str.length >= 11 && str.length <= 14){
-				// cleaning cpf
-				str=str
-					.replace('.','')
-					.replace('.','')
-					.replace('-','')
-					.replace(" ","");  
-				// tudo igual
-				if (!str.split("").every((c: string) => c === str[0])) {
-					try{  
-						let     d1, d2;  
-						let     dg1, dg2, rest;  
-						let     digito;  
-							let     nDigResult;  
-						d1 = d2 = 0;  
-						dg1 = dg2 = rest = 0;  
-							
-						for (let nCount = 1; nCount < str.length -1; nCount++) {  
-							// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-							// 	return false;
-							// } else {
-	
-								digito = parseInt(str.substring(nCount -1, nCount));  							
-								d1 = d1 + ( 11 - nCount ) * digito;  
-				
-								d2 = d2 + ( 12 - nCount ) * digito;  
-							// }
-						};  
-							
-						rest = (d1 % 11);  
-						
-						// se for menor que 2 é 0, senão é 11 menos o resto
-						dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-						d2 += 2 * dg1;  
-						rest = (d2 % 11);  
-						if (rest < 2)  
-							dg2 = 0;  
-						else  
-							dg2 = 11 - rest;  
-				
-							let nDigVerific = str.substring(str.length-2, str.length);  
-						nDigResult = "" + dg1 + "" + dg2;  
+export function validateCpf(str: string) {
+  if (!str) return false;
 
-						return nDigVerific == nDigResult;
-						
-					// se der problema...
-					}catch (e){  
-						console.error("Erro !"+e);  
-	
-						return false;  
-					}  
-				} else return false;
-	
-			}else return false;
-		} else return false;
-	} else return false;
+  str = str.replace(/\D/g, "");
+  if (str.length !== 11 || str.split("").every((c: string) => c === str[0]))
+    return false;
+
+  const calcDigit = (cpf: string, factor: number) =>
+    cpf
+      .slice(0, factor - 1)
+      .split("")
+      .reduce((sum, c, i) => sum + parseInt(c) * (factor - i), 0);
+
+  const mod = (n: number) => (n % 11 < 2 ? 0 : 11 - (n % 11));
+  const dg1 = mod(calcDigit(str, 10));
+  const dg2 = mod(calcDigit(str, 11));
+
+  return str.slice(-2) === `${dg1}${dg2}`;
 }
